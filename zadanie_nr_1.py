3119cef42854f620bf1a668b9d1d878d92d87a3f
Redzyk <rodzynq@gmail.com>
2019-08-02 13:49:48 +0200

    task_1:sollution

--- zadanie_nr_1.py
+++ zadanie_nr_1.py
@@ -1,6 +1,8 @@
 # Make function that will print elements of given list, each one in new line
 
 def print_in_lines(l):
-    pass
+    for num in l:
+        print(num)
+
 
 print_in_lines([1, 2, 3, 4])
