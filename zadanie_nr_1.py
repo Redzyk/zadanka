1c26aa917c8f1859b023d7338d390a567dd65f7d
Redzyk <rodzynq@gmail.com>
2019-08-02 14:04:13 +0200

    task_1: change variable name

--- zadanie_nr_1.py
+++ zadanie_nr_1.py
@@ -1,8 +1,8 @@
 # Make function that will print elements of given list, each one in new line
 
 def print_in_lines(l):
-    for num in l:
-        print(num)
+    for element in l:
+        print(element)
 
 
 print_in_lines([1, 2, 3, 4])
