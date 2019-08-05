4234f330049b4070cf39a3edbe5effb0f9966b16
Redzyk <rodzynq@gmail.com>
2019-08-05 23:27:40 +0200

    details

--- Training.py
+++
@@ -1,8 +0,0 @@
-def sentence(x):
-    for element in x.split():
-        if len(element)%2==0:
-            print (element + " : has even number of letters")
-        else:
-            print ("W morde")
-
-sentence("Piotrek jest słaby")
