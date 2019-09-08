4267f2f5870abd044eb1ed10d888942d01209df0
Redzyk <rodzynq@gmail.com>
2019-09-08 16:21:59 +0200

    weight conversion

--- task_8.py
+++ task_8.py
@@ -1,13 +1,13 @@
 weight = int(input("Weight: "))
-while weight not in range(0,1001):
-    print("Try again")
+while True:
+  unit = input("(K)g or (L)bs? ")
+  if unit.upper() == "K":
+    kg_to_lb = int(weight // 0.45)
+    print(f"It's {kg_to_lb} pounds.")
     break
-unit = input("(K)g or (L)bs: ")
-if unit.upper() == "K":
-    conversion = int(weight // 0.45)
-    print("Your weight is {} pounds".format(conversion))
-if unit.upper() == "L":
-    conversion = int(weight * 0.45)
-    print("Your weight is {} kilos".format(conversion))
-elif unit.upper() != "K" or "L":
-    print("Please use K or L")
+  elif unit.upper() == "L":
+    lb_to_kg = int(weight * 0.45)
+    print(f"It's {lb_to_kg} kilos.")
+    break
+  else:
+    print("""Please use "K" or "L". """)
